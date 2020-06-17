8657058bcf26b3fe4d0c25dd35bbb00d977fe439
Amanda Hogan <anhogan@MacBook-Pro.attlocal.net>
2020-06-17 12:29:18 -0400

    Add in attempt at RPS problem

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -7,6 +7,7 @@ def eating_cookies(n):
     eaten = 0
     if eaten > n:
         return 0
+        
     # If eaten is the number of available cookies, return a count of 1
     if eaten == n:
         return 1
