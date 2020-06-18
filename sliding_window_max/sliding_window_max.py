bba0afbca5bf614ae26a4bc5d77fc6902838d934
Amanda Hogan <anhogan@MacBook-Pro.attlocal.net>
2020-06-18 11:25:10 -0400

    Use memoization to write a better solution for eating cookies problem and pass large tests

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -3,21 +3,42 @@ Input: a List of integers as well as an integer `k` representing the size of the
 Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
-    # Look for the maximum value within k throughout the nums arr and store those values in a result list
-    # If len(k) - 1 > len(nums) - 1 return because the sliding window is no longer fitting within the list
-    # Start at index 0 and create a subarray of len k (slice nums to be nums[index:k])
-    # After each pass increment index by 1
-    # Use recursion to go through the full nums list
-    result = []
-    index = 0
-    end_index = k
+    ''' FIRST PASS SOLUTION '''
 
-    while index <= len(nums) - k:
-        temp_nums = nums[index:end_index]
-        max_value = max(temp_nums)
-        result.append(max_value)
-        index += 1
-        end_index += 1
+    # # Look for the maximum value within k throughout the nums arr and store those values in a result list
+    # result = []
+    # index = 0
+    # end_index = k
+
+    # # If len(k) - 1 > len(nums) - 1 return because the sliding window is no longer fitting within the list
+    # while index <= len(nums) - k:
+    #     # Start at index 0 and create a subarray of len k (slice nums to be nums[index:k])
+    #     temp_nums = nums[index:end_index]
+    #     max_value = max(temp_nums)
+    #     result.append(max_value)
+
+    #     # After each pass increment indexes by 1
+    #     index += 1
+    #     end_index += 1
+
+    # return result
+
+    ''' WRITING BETTER SOLUTIONS '''
+
+    result = []
+
+    # Loop through nums using enumerate to get index at each element
+    for idx,e in enumerate(nums):
+        # Create temp list for nums sliced between index and index + k
+        temp_list = nums[idx:idx + k]
+
+        # If len(temp_list) < k, break loop
+        if len(temp_list) < k:
+            break
+        # Else, find max from that list and append to result list
+        else:
+            max_value = max(temp_list)
+            result.append(max_value)
 
     return result
 
